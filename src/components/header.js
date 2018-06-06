94ea4c2d782017fc14874778ac49558d01869d78
matthew lee <matthewlee07@gmail.com>
2018-06-06 14:59:46 -0400

    header css

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './header.css';
 
 export default function Header(props) {
     return (
-        <header>Header</header>
+        <header className="container">
+            <span id="logo">Travel Battle Logo</span>
+            <span className="right">
+                <a href="#about">About</a>
+                <a href="#account">My Account</a>
+            </span>
+
+        </header>
     )
 }
