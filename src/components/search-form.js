e19e3b09a0d745552c50e9b8e29ddc1280552385
matthew lee <matthewlee07@gmail.com>
2018-06-08 21:51:56 -0400

    import Component to search-form, changed extends Component

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
 import { createSearch } from '../actions';
-export class SearchForm extends React.Component {
+export class SearchForm extends Component {
 
 
     onSubmit(values) {
